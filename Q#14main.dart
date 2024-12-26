f081cfd2531327b47f97d4f71e7a06d4560598e6
Ammar Palh <ammarpalh4@gmail.com>
2024-12-26 08:11:38 +0500

    Update Q#14main.dart

--- Q#14main.dart
+++ Q#14main.dart
@@ -2,5 +2,14 @@
 // the elements sorted in ascending order. The original list should remain
 // unchanged.
 void main(){
-  
+  List<int> digits = [2, 8, 6, 0, 4, 10];
+  List<int> sortedDigits = List.from(digits);
+  sortedDigits.sort();
+  print("Original List: $digits");
+  print("Sorted List: $sortedDigits");
+}
+
+// Answer is ->
+//            Original List: [2, 8, 6, 0, 4, 10]
+//            Sorted List: [0, 2, 4, 6, 8, 10]
 }
