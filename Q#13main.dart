c6026e32020f7b5debb751b720e4c2840ded5994
Ammar Palh <ammarpalh4@gmail.com>
2024-12-26 08:11:08 +0500

    Update Q#13main.dart

--- Q#13main.dart
+++ Q#13main.dart
@@ -2,5 +2,12 @@
 // containing only the unique elements from the original list. The order of
 // elements in the new list should be the same as in the original list.
 void main(){
-  
+   List<int> digits = [5, 10, 15, 5, 18, 20, 25, 18, 10];
+  List uniqueDigits = digits.toSet().toList();
+  print(uniqueDigits);
+
+}
+
+// Answer is ->
+//            [5, 10, 15, 18, 20, 25]
 }
