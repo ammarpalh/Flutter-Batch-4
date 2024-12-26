718d321689760611877046445bad121292829589
Ammar Palh <ammarpalh4@gmail.com>
2024-12-26 08:13:58 +0500

    Update Q#19main.dart

--- Q#19main.dart
+++ Q#19main.dart
@@ -2,5 +2,19 @@
 // "quantity", write Dart code to check if the product is in stock. If the quantity is
 // greater than 0, print "In stock", otherwise print "Out of stock".
 void main(){
-  
+   Map<String, dynamic> product = {
+    "name": "Iphone12",
+    "price": "400 Dollars",
+    "quantity": 4
+  };
+  int quantity = product["quantity"];
+  if (quantity > 0) {
+    print("In stock");
+  } else {
+    print("Out of stock");
+  }
+}
+
+// Answer is ->
+//             In stock
 }
