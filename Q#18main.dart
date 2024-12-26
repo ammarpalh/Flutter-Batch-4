5d52f12d2b348a2f57d59d98c3c442c2d44efd4c
Ammar Palh <ammarpalh4@gmail.com>
2024-12-26 08:13:24 +0500

    Update Q#18main.dart

--- Q#18main.dart
+++ Q#18main.dart
@@ -3,5 +3,14 @@
 // person is both a student and over 18 years old. Print "Eligible" if both
 // conditions are true, otherwise print "Not eligible".
 void main(){
-  
+   Map person = {"name": "John", "age": 25, "isStudent": true};
+  if (person["isStudent"] == true && person["age"] > 18) {
+    print("Eligible");
+  } else {
+    print("Not Eligible");
+  }
+}
+
+  // Answer is ->
+  //             Eligible
 }
