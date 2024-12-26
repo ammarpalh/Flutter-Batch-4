525b6612c8c149d908380de395fbc0efd29e7839
Ammar Palh <ammarpalh4@gmail.com>
2024-12-26 08:12:56 +0500

    Update Q#17main.dart

--- Q#17main.dart
+++ Q#17main.dart
@@ -2,5 +2,14 @@
 // create a new list with each value squared. The program should take in the
 // original list as a parameter and print the new list.
 void main(){
-  
+   List <int> digits = [2, 4, 6, 8];
+  List <int> squaredDigits = digits.map((digit) => digit*digit).toList();
+  print("Original Values: $digits");
+  print("Squared Values: $squaredDigits");
+
+}
+
+// Answer is ->
+//             Original Values: [2, 4, 6, 8]
+//             Squared Values: [4, 16, 36, 64]
 }
