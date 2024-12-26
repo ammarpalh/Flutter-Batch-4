7e5d9a9212d46f3ff49fd78fbddae13c21c23d1c
Ammar Palh <ammarpalh4@gmail.com>
2024-12-26 08:12:04 +0500

    Update Q#15main.dart

--- Q#15main.dart
+++ Q#15main.dart
@@ -2,5 +2,12 @@
 // numbers from a list of integers. The program should take in the original list as a
 // parameter and print a new list containing only the positive numbers.
 void main(){
-  
+   List<int> digits = [2, -5, 3, -2, -4, 5];
+  List<int> positiveDigits = digits.where((digit) => (digit > 0)).toList();
+  print("Positive Numbers: $positiveDigits");
+
+}
+
+// Answer is ->
+//             Positive Numbers: [2, 3, 5]
 }
